0fcd8d694c7a9c09e3533328e300eb3ca8663e4a
esim-desim <esim.desim@gmail.com>
2025-01-12 18:52:20 +0100

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,1 +1,19 @@
+export default async function handler(req, res) {
+  // Dobijamo URL kao query parametar
+  const { url } = req.query;
 
+  if (!url) {
+    return res.status(400).json({ error: 'URL parameter is missing' });
+  }
+
+  try {
+    // Pravimo fetch ka ciljanom URL-u
+    const response = await fetch(url);
+    const data = await response.json();
+
+    // Vraćamo podatke kao JSON
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching data' });
+  }
+}
