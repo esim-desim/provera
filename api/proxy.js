e160fdaf7c1c2801b9fa4dbb0c15c7fe5ac49190
esim-desim <esim.desim@gmail.com>
2026-02-13 13:18:27 +0100

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -19,7 +19,7 @@ export default async function handler(req, res) {
         const response = await fetch(url, {
             method: 'GET',
             headers: {
-                'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6MTk1MDc2fQ.eyJzdWIiOjE5NTA3NiwiaXNzIjoiIiwiYXVkIjoiIiwiaWF0IjoxNzM2NTU5ODI3LCJleHAiOjE3NjgwOTU4MjcsImp0aSI6IjIzNTlmY2VkMWE0NjViZCIsImlkIjoxOTUwNzZ9.w4gJxi2AHlx-3r-Hu3w-Wvd8XIIOyVFnto_hnGQXAvI',
+                'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6MTk1MDc2fQ.eyJzdWIiOjE5NTA3NiwiaXNzIjoiIiwiYXVkIjoiIiwiaWF0IjoxNzcwOTg0OTU2LCJleHAiOjM1NzI3MjgzMTIsImp0aSI6ImIwMzFmYjBhMGFiOTU4MSIsImlkIjoxOTUwNzYsInRva2VuX3ZlcnNpb24iOjF9.BGNAy6AWIJz8yamz3Rg2piJr23cJJCpKsfEj-z7Jsf8',
                 'Accept': 'application/json',
             }
         });
